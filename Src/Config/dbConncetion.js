16998b2a4b4a753053e2b4b39a4ecac8863fef9f
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-10-25 21:58:00 +0530

    done changes for testing

--- Src/Config/dbConncetion.js
+++ Src/Config/dbConncetion.js
@@ -1,16 +1,16 @@
 const {Pool} =require('pg')
 
-// const pool=new Pool({
-//   user: process.env.PG_USER,          
-//   host: process.env.PG_HOST,
-//   database: process.env.PG_DATABASE,
-//   password: process.env.PG_PASSWORD,
-//   port: process.env.PG_PORT,
-// })
-
-const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
+const pool=new Pool({
+  user: process.env.PG_USER,          
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 })
+
+// const pool = new Pool({
+//   connectionString: process.env.POSTGRES_URL,
+// })
 
 const connectDB=async()=>{
     try{
