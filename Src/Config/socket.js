88f911dc734c666e87ff46730641fc5480fdabb4
abdulhakeem1232 <hakeemforgit@gmail.com>
2024-10-25 19:28:09 +0530

    socket testing

--- Src/Config/socket.js
+++ Src/Config/socket.js
@@ -12,7 +12,7 @@ const initializeSocket = (server) => {
 
   const io = require('socket.io')(server, {
     cors: {
-      origin: 'https://kiara-global-service.vercel.app',
+      origin: '*',
       methods: ['GET', 'POST'],
       credentials: true
     }
